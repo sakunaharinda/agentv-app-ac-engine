06bfb479f60b7d3ecae0efda6bd1eb6d7b6b6ed6
Sakuna Jayasundara <sakunaj1996@gmail.com>
2025-04-26 11:26:30 +0000

    Updated docker files

--- docker_build_run.sh
+++ docker_build_run.sh
@@ -1,4 +1,4 @@
-./mvnw package && java -jar target/agentv-app-ac-engine-1.0.jar
+mvn clean package -DskipTests
 docker build -t agentv-app-ac-engine .
 docker run --name agentv-app-ac-engine -p 8081:8081 agentv-app-ac-engine
 docker logs -f agentv-app-ac-engine
